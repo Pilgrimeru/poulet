1feb74fb54751ab791845217d8f144fc62c39673
Pilgrimeru <84448802+Pilgrimeru@users.noreply.github.com>
2026-03-29 11:30:38 +0200

    feat(channelCreate): ✨ Implement channel creation event handling
    * Added logic to handle channel creation events.
    * Upsert channel metadata including ID, guild ID, name, parent ID, parent name, and type.
    * Removed console log for cleaner output.

--- src/discord/events/channelCreate.ts
+++ src/discord/events/channelCreate.ts
@@ -1,6 +1,10 @@
 import { GuildChannel } from "discord.js";
 import { Event } from "@/discord/types";
+import { channelMetaService } from "@/api";
 
 export default new Event("channelCreate", async (channel: GuildChannel) => {
-  console.log(`channelCreate: ${channel}`);
+  if (!("name" in channel) || !channel.name) return;
+  const parentID = "parentId" in channel ? channel.parentId ?? null : null;
+  const parentName = "parent" in channel && channel.parent ? channel.parent.name : null;
+  await channelMetaService.upsert(channel.id, channel.guild.id, channel.name, parentID, parentName, channel.type);
 });
